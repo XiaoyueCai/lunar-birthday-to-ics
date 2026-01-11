101ea5602d5956ae1c957154c1c26228d2e9a9c8
xiao <645367852@qq.com>
2026-01-11 19:14:33 +0800

    skip leap month

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def die(err):
 
 def json_to_persons(json_file_path:str) -> list[Person]:
     ret = []
-    with open(json_file_path, 'r') as f:
+    with open(json_file_path, 'r', encoding='utf-8') as f:
         # json key
         KEY_PERSONS='persons'
         KEY_NAME = 'name'
@@ -60,40 +60,32 @@ def append_birthday_to_calendar(calendar, person:Person, this_year, max_age) -> 
     if age <= 0 or age > max_age:
         return False
 
-    new_birthday_solars = []
-
-    # birthday which is not leap month
-    new_birthday_lunar = lunarcalendar.Lunar(this_year, person.lunar_month, person.lunar_day, isleap = False)
-    new_birthday_solars.append(lunarcalendar.Converter.Lunar2Solar(new_birthday_lunar))
-
-    # birthday which is leap month
     try:
-        new_birthday_lunar = lunarcalendar.Lunar(this_year, person.lunar_month, person.lunar_day, isleap = True)
-        new_birthday_solars.append(lunarcalendar.Converter.Lunar2Solar(new_birthday_lunar))
-        # print(f'There is a leap month in {solar_year}-{lunar_month}')
-    except lunarcalendar.DateNotExist:
-        # print(f'Not exist for leap month in {solar_year}-{lunar_month}')
-        pass
+        # 始终指定 isleap=False，只获取正式月份的日期
+        new_birthday_lunar = lunarcalendar.Lunar(this_year, person.lunar_month, person.lunar_day, isleap=False)
+        new_birthday_solar = lunarcalendar.Converter.Lunar2Solar(new_birthday_lunar)
 
-    for i in range(len(new_birthday_solars)):
-        new_birthday_solar = new_birthday_solars[i]
-
+        # 创建日历事件
         icsEvent = ics.Event()
         icsEvent.name = f'{person.name}的农历{age}岁生日'
-        if i > 0:
-            icsEvent.name += '(闰)'
         icsEvent.description = f'生日快乐，{person.year}年出生，属{person.zodiac()}，农历{person.lunar_month:02d}-{person.lunar_day:02d}'
         icsEvent.begin = datetime.datetime(new_birthday_solar.year, new_birthday_solar.month, new_birthday_solar.day)
         icsEvent.make_all_day()
         icsEvent.created = datetime.datetime.now()
         calendar.events.add(icsEvent)
 
+    except lunarcalendar.DateNotExist:
+        # 这种情况通常发生在：出生在农历30日，但这年的该农历月份只有29天（小月）
+        # 此时跳过该年，不生成事件
+        # print(f'Skipping {person.name} in {this_year} due to non-existent lunar date (e.g. 30th in short month)')
+        pass
+
     return True
 
 if __name__ == "__main__":
     script_file = os.path.basename(sys.argv[0])
     parser = argparse.ArgumentParser(prog=script_file,
-                                     formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=80),
+                                     formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=80),
                                      description='A simple script to generate chinese lunar calendar birthday')
     parser.add_argument('-i', metavar='config.json', help='The input config file', required=True)
     parser.add_argument('-c', metavar='COUNT', help='The number of occurrences(in years), default value: 50', default=50, type=int)
